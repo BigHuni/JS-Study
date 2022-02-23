a8b29c7b8f92cf295a55d93ef6d3ed9ec057d872
BigHuni <hdh8659@gmail.com>
2022-02-23 23:42:17 +0900

    [add] modal 과제 초안 작성

---
+++ Assignment/5회차/main.js
@@ -0,0 +1,6 @@
+const modal = document.querySelector('.modal');
+const btnopen = document.querySelector('.btn-open');
+
+btnopen.addEventListener('click', () => {
+    modal.style.display = 'block';
+});
