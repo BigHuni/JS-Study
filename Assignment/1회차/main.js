22b74f72a6457f2eddceb6a5d5c9bd251e4a9dff
BigHuni <hdh8659@gmail.com>
2022-02-26 10:32:07 +0900

    [refactor] 로컬 스토리지 전반적인 리팩토링 작업

--- Assignment/1회차/main.js
+++ Assignment/1회차/main.js
@@ -1,55 +1,35 @@
-const form = document.querySelector(".js-form");
-const input = form.querySelector("input");
-const greeting = document.querySelector(".js-greeting");
-const button = document.querySelector("button"); 
+const form = document.querySelector("form");
+const ul = document.querySelector("ul");
+const button = document.querySelector("button");
+const text = document.querySelector("textarea");
+const input = document.getElementById("item");
+const clear = document.getElementById("clear");
+let itemsArray = localStorage.getItem("items") ? JSON.parse(localStorage.getItem("items")) : [];
 
-//저장할 데이터의 key값 
-const LOCAL_DATA = "Contents"; 
+localStorage.setItem("items", JSON.stringify(itemsArray));
+const data = JSON.parse(localStorage.getItem("items"));
 
-//input태그에 이름 입력 후 input 태그를 감추고, 텍스트가 나타나게 해주는 함수 
-function hiddenAndGreeting(name) { 
-    greeting.style.display = "block";
-    form.style.display = "none"; 
-    button.style.display = "block"; 
-    button.addEventListener("click", onClick); 
-    greeting.innerText = `메모: ${name}`; } 
-    
-//버튼 클릭 시, 데이터 삭제 이벤트 발생 
-function onClick() { 
-    localStorage.removeItem(LOCAL_DATA); 
-    console.log("hi"); 
-    loaded(); 
-} 
+const liMaker = (text) => {
+    const li = document.createElement("li");
+    li.textContent = text;
+    ul.appendChild(li);
+};
 
-//input태그에 입력 시, lacalStorage의 value 값으로 저장 
-function onSubmit(e) {
-    e.preventDefault(); 
-    localStorage.setItem(LOCAL_DATA, input.value); 
-    hiddenAndGreeting(input.value); 
-    input.value = ""; 
-} 
-        
-//input태그 보이게 설정, 인사 텍스트와 버튼은 숨김
-function askForContents() { 
-    form.style.display = "block"; 
-    greeting.style.display = "none"; 
-    button.style.display = "none"; 
-    form.addEventListener("submit", onSubmit); //form에 submit 이벤트 추가 
-} 
+form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    itemsArray.push(text.value);
+    localStorage.setItem("items", JSON.stringify(itemsArray));
+    liMaker(text.value);
+    input.value = "";
+});
 
-//브라우저의 localStorage에 데이터가 있을 때와 없을 때를 구분하여 실행 시켜줄 함수
-function loaded() { 
-    const data = localStorage.getItem(LOCAL_DATA); 
-    if (data === null) { // 데이터가 없을 시 
-        askForContents(); 
-    } else { //데이터가 이미 있을 시 
-        hiddenAndGreeting(data); 
-    } 
-} 
+data.forEach((item) => {
+    liMaker(item);
+});
 
-//처음 실행할 init 함수 
-function init() { 
-    loaded(); 
-} 
-
-init();
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    while (ul.firstChild) {
+        ul.removeChild(ul.firstChild);
+    }
+});
